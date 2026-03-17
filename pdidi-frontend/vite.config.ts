84255844e821e5eb325262e74ccc18c72c5454fa
reygish <danielregisf@gmail.com>
2026-03-17 11:13:28 +0700

    fix: env example for frontend and necesarry ignores for deploy

--- pdidi-frontend/vite.config.ts
+++ pdidi-frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://dregis.site/', // command this if for local
   server: {
     watch: {
       usePolling: true,
