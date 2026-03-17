b600f17155ecd46e22778b97cb14c81bac9bb32f
reygish <danielregisf@gmail.com>
2026-03-17 11:21:10 +0700

    fix: base url for deployment

--- pdidi-frontend/vite.config.ts
+++ pdidi-frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://dregis.site/', // command this if for local
+  base: 'https://dregis.site/catering/', // command this if for local
   server: {
     watch: {
       usePolling: true,
