81d6368a807373203bd57eba5a5a03cd6521f49e
reygish <danielregisf@gmail.com>
2026-03-27 14:58:50 +0700

    feat: dynamic config for prod and dev, added og image metadata, updated env example

--- pdidi-frontend/vite.config.ts
+++ pdidi-frontend/vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-  //base: 'https://dregis.site/catering/', // command this if for local
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: command === 'build' ? env.BASE_URL : '/',
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: "0.0.0.0",
+      port: 5173
     },
-    host: "0.0.0.0",
-    port: 5173
-  },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
     }
   }
 })
